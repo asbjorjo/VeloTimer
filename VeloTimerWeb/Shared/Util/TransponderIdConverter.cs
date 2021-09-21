bfcc688e20924d666c9770eee534474469e988d5
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-09-21 16:23:21 +0200

    Revert "Make internal properties private. Introduce constants for the constant values used in computations."
    This reverts commit e40d00588fa378eeb689df4c7e55f4f6d1ae5349.

--- VeloTimerWeb/Shared/Util/TransponderIdConverter.cs
+++ VeloTimerWeb/Shared/Util/TransponderIdConverter.cs
@@ -4,20 +4,17 @@ namespace VeloTimer.Shared.Util
 {
     public class TransponderIdConverter
     {
-        private const int idOffset = 0x6000000;
-        private const int numericOffset = 100000;
-
-        private static readonly char[] Characters = { 'C', 'F', 'G', 'H', 'K', 'L', 'N', 'P', 'R', 'S', 'T', 'V', 'W', 'X', 'Z' };
+        public static char[] Characters { get; } = { 'C', 'F', 'G', 'H', 'K', 'L', 'N', 'P', 'R', 'S', 'T', 'V', 'W', 'X', 'Z' };
 
         public static string IdToCode(long Id)
         {
             string code = "";
 
-            if (Id > idOffset)
+            if (Id > 0x6000000)
             {
                 string prefix = "";
-                var n = Id - idOffset;
-                var m = n / numericOffset;
+                var n = Id - 0x6000000;
+                var m = n / 100000;
 
                 for (int i = 2; i >= 0; i--)
                 {
@@ -28,8 +25,9 @@ namespace VeloTimer.Shared.Util
                         m = m / 15;
                     }
                 }
+                var suffix = String.Format("{0:00000}", n % 100000);
 
-                code = $"{prefix}-{n % numericOffset:00000}";
+                code = $"{prefix}-{suffix}";
             }
 
             return code;
@@ -52,12 +50,12 @@ namespace VeloTimer.Shared.Util
                 && Array.Exists(Characters, c => codeArray[1] == c)
                 && Array.Exists(Characters, c => codeArray[2] == c))
             {
-                id = numericOffset
+                id = 100000
                      * (Array.IndexOf(Characters, codeArray[0]) * 225
                         + Array.IndexOf(Characters, codeArray[1]) * 15
                         + Array.IndexOf(Characters, codeArray[2]));
                 id += int.Parse(code.Substring(3, 5));
-                id += idOffset;
+                id += 0x6000000;
             }
 
             return id;
