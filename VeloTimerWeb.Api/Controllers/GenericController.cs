55139cedf2ac70115db2523f0ed6174f41d0aa2f
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-09-29 16:25:21 +0200

    Remove unused System import.

--- VeloTimerWeb.Api/Controllers/GenericController.cs
+++ VeloTimerWeb.Api/Controllers/GenericController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
