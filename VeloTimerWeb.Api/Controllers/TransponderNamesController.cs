220dead5e2b0b9e0380b12c9d601da89a53de50c
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-10-01 13:16:46 +0200

    For now we don't want anyone adding a bunch of transponder names at once.

--- VeloTimerWeb.Api/Controllers/TransponderNamesController.cs
+++ VeloTimerWeb.Api/Controllers/TransponderNamesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,11 @@ namespace VeloTimerWeb.Api.Controllers
         public TransponderNamesController(ILogger<GenericController<TransponderName>> logger,
                                           ApplicationDbContext context) : base(logger, context)
         {
+        }
+
+        public override Task<ActionResult> Create(IEnumerable<TransponderName> values)
+        {
+            throw new NotImplementedException($"Batch creation of {nameof(TransponderName)} not supported");
         }
     }
 }
