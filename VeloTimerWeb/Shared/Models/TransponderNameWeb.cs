b8331eb034165745507737f42f106898983b4bbf
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-10-01 16:33:24 +0200

    Introduce separate model for API calls.

---
+++ VeloTimerWeb/Shared/Models/TransponderNameWeb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VeloTimer.Shared.Models.Validation;
+
+namespace VeloTimer.Shared.Models
+{
+    public class TransponderNameWeb
+    {
+        [Required]
+        public string TransponderLabel { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTimeOffset ValidFrom { get; set; }
+        [EndDate(otherPropertyName = nameof(ValidFrom))]
+        public DateTimeOffset ValidUntil { get; set; }
+    }
+}
