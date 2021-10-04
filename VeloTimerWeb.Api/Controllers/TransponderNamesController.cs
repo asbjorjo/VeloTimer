2d12ac03a18799b08ce2f428778a8e41d5424826
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-10-04 17:24:12 +0200

    Add navigation property to transponder names and code to check for conflicts before insert. Also use in segment times list.

--- VeloTimerWeb.Api/Controllers/TransponderNamesController.cs
+++ VeloTimerWeb.Api/Controllers/TransponderNamesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,26 @@ namespace VeloTimerWeb.Api.Controllers
         public TransponderNamesController(ILogger<GenericController<TransponderName>> logger,
                                           ApplicationDbContext context) : base(logger, context)
         {
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public override async Task<ActionResult<TransponderName>> Create(TransponderName value)
+        {
+            _logger.LogInformation($"Transponder: {value.TransponderId}" +
+                $" - Name: {value.Name}" +
+                $" - Validity: {value.ValidFrom}-{value.ValidUntil}");
+
+            var existing = _dbset.Where(tn => (value.ValidFrom >= tn.ValidFrom && value.ValidFrom <= tn.ValidUntil)
+                                              || (value.ValidUntil >= tn.ValidFrom && value.ValidUntil <= tn.ValidUntil)
+                                              || (value.ValidFrom <= tn.ValidFrom && value.ValidUntil >= tn.ValidUntil));
+
+            if (existing.Any())
+            {
+                return Conflict();
+            }
+
+            return await base.Create(value);
         }
 
         public override Task<ActionResult> Create(IEnumerable<TransponderName> values)
