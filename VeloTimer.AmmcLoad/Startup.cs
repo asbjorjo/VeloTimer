04523b1ffa5e221f81dd49096cd66fc59656e4ec
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-10-07 19:45:34 +0200

    Remove the console refresh application and move to separate empty web app.

---
+++ VeloTimer.AmmcLoad/Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using VeloTimer.AmmcLoad.Data;
+using VeloTimer.AmmcLoad.Services;
+
+namespace VeloTimer.AmmcLoad
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<PassingDatabaseSettings>(
+                                         Configuration.GetSection(nameof(PassingDatabaseSettings)));
+            services.AddSingleton<IPassingDatabaseSettings>(sp =>
+                        sp.GetRequiredService<IOptions<PassingDatabaseSettings>>().Value);
+            services.AddSingleton<AmmcPassingService>();
+            services.AddHttpClient("VeloTimerWeb.ServerAPI", client => client.BaseAddress = new Uri("https://velotimer-api.azurewebsites.net"));
+            services.AddSingleton(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("VeloTimerWeb.ServerAPI"));
+            services.AddHostedService<RefreshPassingsService>();
+
+            services.AddRazorPages();
+            services.AddApplicationInsightsTelemetry();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
