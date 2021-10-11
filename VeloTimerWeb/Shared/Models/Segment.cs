8722f0a78a301abb084dd525424ef7fb27cdbc33
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-10-11 17:49:33 +0200

    Add explicit ids for start and end.

--- VeloTimerWeb/Shared/Models/Segment.cs
+++ VeloTimerWeb/Shared/Models/Segment.cs
@@ -9,10 +9,12 @@ namespace VeloTimer.Shared.Models
     public class Segment : Entity
     {
         public string Label { get; set; }
+        public long StartId { get; set; }
+        public long EndId { get; set; }
 
         public TimingLoop Start { get; set; }
         public TimingLoop End { get; set; }
 
-        public IEnumerable<TimingLoop> Intermediates { get; set; }
+        public virtual ICollection<TimingLoop> Intermediates { get; set; }
     }
 }
