b5510f74bd67ec91ae619ab85dec1bd555fe8a3e
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-10-26 17:28:28 +0200

    Add component to display list of active transponders.

--- VeloTimerWeb.Api/Controllers/TranspondersController.cs
+++ VeloTimerWeb.Api/Controllers/TranspondersController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using VeloTimer.Shared.Models;
 using VeloTimerWeb.Api.Data;
 
@@ -8,6 +14,15 @@ namespace VeloTimerWeb.Api.Controllers
     {
         public TranspondersController(ILogger<GenericController<Transponder>> logger, ApplicationDbContext context) : base(logger, context)
         {
+        }
+
+        [Route("active")]
+        [HttpGet]
+        public async Task<ActionResult<ICollection<Transponder>>> GetActive()
+        {
+            var value = _dbset.Where(t => t.Passings.Where(p => p.Time > DateTime.Now.AddHours(-1)).Any()).OrderByDescending(t => t.Passings.OrderByDescending(p => p.Time).First());
+            
+            return await value.ToListAsync();
         }
     }
 }
