587e2fd8c0289e6ecd1d3cc550ec2c871b1f763c
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-10-31 11:42:07 +0100

    Start using DateTimeOffset in services and fix controller code to act accordingly.

--- VeloTimerWeb.Api/Services/ISegmentService.cs
+++ VeloTimerWeb.Api/Services/ISegmentService.cs
@@ -7,7 +7,7 @@ namespace VeloTimerWeb.Api.Services
 {
     public interface ISegmentService
     {
-        Task<IEnumerable<SegmentTimeRider>> GetSegmentTimesAsync(long segment, long? transponder, DateTime? fromtime, TimeSpan? period);
-        Task<long> GetSegmentPassingCountAsync(long segment, long? transponder, DateTime? fromtime, TimeSpan? period);
+        Task<IEnumerable<SegmentTimeRider>> GetSegmentTimesAsync(long segment, long? transponder, DateTimeOffset? fromtime, TimeSpan? period);
+        Task<long> GetSegmentPassingCountAsync(long segment, long? transponder, DateTimeOffset? fromtime, TimeSpan? period);
     }
 }
