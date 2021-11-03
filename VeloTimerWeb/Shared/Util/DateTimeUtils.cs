ad05a7318632f6456d43cd670bc8d9c00446f997
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-11-03 14:15:03 +0100

    DateTime calculations are the worst.

---
+++ VeloTimerWeb/Shared/Util/DateTimeUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeloTimer.Shared.Util
+{
+    public class DateTimeUtils
+    {
+        public static DateTimeOffset StartOfMonth(DateTimeOffset date)
+        {
+            return new DateTimeOffset(date.Year, date.Month, 1, 0, 0, 0, date.Offset);
+        }
+
+        public static DateTimeOffset EndOfMonth(DateTimeOffset date)
+        {
+            return StartOfMonth(date).AddMonths(1).AddDays(-1);
+        }
+    }
+}
