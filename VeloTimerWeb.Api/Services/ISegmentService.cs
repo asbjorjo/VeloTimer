cf0394d879dd45f1cace18cbe392b64f07f1c849
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-11-16 14:49:27 +0100

    Get number of segment passings in a better way and remove async from function names.

--- VeloTimerWeb.Api/Services/ISegmentService.cs
+++ VeloTimerWeb.Api/Services/ISegmentService.cs
@@ -9,7 +9,7 @@ namespace VeloTimerWeb.Api.Services
     {
         Task<IEnumerable<SegmentTimeRider>> GetFastestSegmentTimes(long segmentId, long? transponderId, DateTimeOffset? fromtime, DateTimeOffset? totime, int? count, bool requireintermediates);
         Task<IEnumerable<SegmentTimeRider>> GetFastestSegmentTimesNewWay(long segmentId, long? transponderId, DateTimeOffset? fromtime, DateTimeOffset? totime, int? count, bool requireintermediates);
-        Task<IEnumerable<SegmentTimeRider>> GetSegmentTimesAsync(long segment, long? transponder, DateTimeOffset? fromtime, DateTimeOffset? totime);
-        Task<IEnumerable<KeyValuePair<string, long>>> GetSegmentPassingCountAsync(long segment, long? transponder, DateTimeOffset? fromtime, DateTimeOffset? totime);
+        Task<IEnumerable<SegmentTimeRider>> GetSegmentTimes(long segment, long? transponder, DateTimeOffset? fromtime, DateTimeOffset? totime);
+        Task<IEnumerable<KeyValuePair<string, long>>> GetSegmentPassingCount(long segment, long? transponder, DateTimeOffset? fromtime, DateTimeOffset? totime, int? count);
     }
 }
