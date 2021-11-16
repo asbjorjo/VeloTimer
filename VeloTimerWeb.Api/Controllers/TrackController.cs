12f504788c6edcbe0108d68ad131deb5471398c1
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-11-16 16:19:39 +0100

    Add controller for track.

---
+++ VeloTimerWeb.Api/Controllers/TrackController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VeloTimer.Shared.Models;
+using VeloTimerWeb.Api.Data;
+
+namespace VeloTimerWeb.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TrackController : GenericController<Track>
+    {
+        public TrackController(ILogger<GenericController<Track>> logger, VeloTimerDbContext context) : base(logger, context)
+        {
+        }
+    }
+}
