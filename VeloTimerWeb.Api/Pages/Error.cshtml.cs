3837f47136e98f25f792e9ad125edb772aed1310
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-11-22 16:14:41 +0100

    Change namespace from obvious template to our own.

--- VeloTimerWeb.Api/Pages/Error.cshtml.cs
+++ VeloTimerWeb.Api/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BlazorApp4.Server.Pages
+namespace VeloTimerWeb.Api.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
