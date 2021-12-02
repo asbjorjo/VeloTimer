f49b166a85a1c233eb4e813ddd63b09926fbe561
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-12-02 14:28:34 +0100

    Add utility method to format times as we are now in the minute range of things.

---
+++ VeloTimerWeb/Shared/Util/Formatters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeloTimer.Shared.Util
+{
+    public class Formatters
+    {
+        public static string FormatTime(double time)
+        {
+            if (time < 60)
+            {
+                return string.Format("{0:00.000}", time);
+            }
+            else
+            {
+                var minutes = time / 60;
+                var seconds = time % 60;
+
+                return string.Format("{0:0}:{1:00.000}", minutes, seconds);
+            }
+        }
+    }
+}
