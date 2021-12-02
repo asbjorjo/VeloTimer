3031cf4c3a04e6349656bd8ffeeabb7aeb341c88
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-12-02 12:03:21 +0100

    Update info screen displays.

--- VeloTimerWeb.Api/Pages/InfoScreen/LapTimes.cshtml.cs
+++ VeloTimerWeb.Api/Pages/InfoScreen/LapTimes.cshtml.cs
@@ -15,27 +15,35 @@ namespace VeloTimerWeb.Api.Pages.InfoScreen
     public class LapTimesModel : PageModel
     {
         private readonly VeloTimerDbContext _context;
+        private readonly ITrackService _service;
 
-        //public Queue<SegmentTimeRider> times { get; set; }
+        public Queue<SegmentTime> times { get; set; }
         
-        public LapTimesModel(VeloTimerDbContext context)
+        public LapTimesModel(ITrackService trackService, VeloTimerDbContext context)
         {
+            _service = trackService;
             _context = context;
         }
 
-        public async Task<IActionResult> OnGetAsync(string SegmentLabel)
+        public async Task<IActionResult> OnGetAsync(string Track, string Label)
         {
-            //segment = await _context.Set<Segment>().SingleOrDefaultAsync(s => s.Label == SegmentLabel);
+            var track = await _context.Set<Track>().FindAsync(long.Parse(Track));
+            if (track == null)
+            {
+                return NotFound($"Track: {Track}");
+            }
 
-            //if (segment == null)
-            //{
-            //    return NotFound();
-            //}
+            var statsitem = await _context.Set<TrackStatisticsItem>().SingleOrDefaultAsync(x => x.Segments.First().Segment.Start.Track == track && x.StatisticsItem.Label == Label);
 
-            //ViewData["Title"] = SegmentLabel;
+            if (statsitem == null)
+            {
+                return NotFound($"StatisticsItem: {Label}");
+            }
 
-            //var seedtimes = await _segmentService.GetSegmentTimes(segment.Id, DateTimeOffset.Now.AddDays(-1), null, 35);
-            //times = new Queue<SegmentTimeRider>(seedtimes);
+            ViewData["Title"] = Label;
+
+            var seedtimes = await _service.GetRecent(statsitem, DateTimeOffset.Now.AddDays(-7), DateTimeOffset.MaxValue, 35);
+            times = new Queue<SegmentTime>(seedtimes);
 
             return Page();
         }
