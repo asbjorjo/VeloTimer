a6a5fbc70f4a8cfaa14102886662abfc38c3fe34
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-12-12 21:45:38 +0100

    Fix oidc configuration path too.

--- VeloTimerWeb/Client/Program.cs
+++ VeloTimerWeb/Client/Program.cs
@@ -42,7 +42,7 @@ namespace VeloTimerWeb.Client
 
             builder.Services.AddApiAuthorization(options =>
             {
-                builder.Configuration.Bind("oidc", options.ProviderOptions);
+                options.ProviderOptions.ConfigurationEndpoint = builder.HostEnvironment.BaseAddress + "/_configuration / VeloTimerWeb.Client";
             })
                 .AddAccountClaimsPrincipalFactory<RolesClaimsPrincipalFactory>();
 
