9655dc87078a1811a1a5d2dd2ddd079be2b1dd82
Asbjoern L. Johansen <asbjorjo@gmail.com>
2021-12-30 15:52:42 +0100

    Remove unused properties, add method to create form from entity.

--- VeloTimerWeb/Client/Models/TransponderOwnershipWebForm.cs
+++ VeloTimerWeb/Client/Models/TransponderOwnershipWebForm.cs
@@ -1,20 +1,38 @@
 ﻿using MudBlazor;
 using System;
 using System.ComponentModel.DataAnnotations;
+using VeloTimer.Shared.Models;
 using VeloTimer.Shared.Util;
 
 namespace VeloTimerWeb.Client.Models
 {
     public class TransponderOwnershipWebForm
     {
-        private DateTime _ownedFrom = DateTime.Now.StartOfDay();
-        private DateTime _ownedUntil = DateTime.Now.AddDays(365).EndOfDay();
-
         [Required]
         public string TransponderLabel { get; set; }
         [Required]
         public string Owner { get; set; }
         [Required]
         public DateRange ValidPeriod { get; set; } = new DateRange(DateTime.Now.StartOfDay(), DateTime.Now.AddDays(365).EndOfDay());
+
+        public static TransponderOwnershipWebForm CreateFromOwnership(TransponderOwnershipWeb transponderOwnership)
+        {
+            if (transponderOwnership == null) throw new ArgumentNullException(nameof(transponderOwnership));
+            if (transponderOwnership.Owner == null) throw new ArgumentNullException("Missing transponder Owner");
+
+            var form = new TransponderOwnershipWebForm { Owner = transponderOwnership.Owner};
+
+            if (!string.IsNullOrWhiteSpace(transponderOwnership.TransponderLabel))
+            {
+                form.TransponderLabel = transponderOwnership.TransponderLabel;
+            }
+
+            if (transponderOwnership.OwnedFrom != default && transponderOwnership.OwnedUntil != default)
+            {
+                form.ValidPeriod = new DateRange(transponderOwnership.OwnedFrom.DateTime, transponderOwnership.OwnedUntil.DateTime);
+            }
+
+            return form;
+        }
     }
 }
