e0ad3f67547632fa6148cef29ccefb4017e5833a
Asbjoern L. Johansen <asbjorjo@gmail.com>
2022-01-07 14:41:12 +0100

    EF Core will not do count(distinct bleh) after groupby, so lame workaround needed.

--- VeloTimerWeb.Api/Controllers/DashboardsController.cs
+++ VeloTimerWeb.Api/Controllers/DashboardsController.cs
@@ -35,15 +35,29 @@ namespace VeloTimerWeb.Api.Controllers
 
             var qDailyCounts = _context.Set<TransponderStatisticsItem>()
                 .Where(x => x.StatisticsItem.StatisticsItem.IsLapCounter)
-                .Where(x => x.EndTime > fromDate)
-                .GroupBy(x => new { Date = x.EndTime.Date })
+                .Where(x => x.StartTime > fromDate)
+                .GroupBy(x => new { Date = x.StartTime.Date })
                 .OrderBy(x => x.Key.Date)
-                .Select(x => new { Date = x.Key.Date, Count = x.Count() });
+                .Select(x => new { Date = x.Key.Date, Count = x.Count()});
+
+            var qRiderCounts = from tsi in _context.Set<TransponderStatisticsItem>()
+                               where tsi.StatisticsItem.StatisticsItem.IsLapCounter && tsi.StartTime > fromDate
+                               group tsi by new {Date = tsi.StartTime.Date, Transponder = tsi.Transponder.Id} into days
+                               orderby days.Key.Date ascending
+                               select new {
+                                   Date = days.Key.Date,
+                                   Transponder = days.Key.Transponder
+                               };
+
+            _logger.LogInformation(qDailyCounts.ToQueryString());
+            _logger.LogInformation(qRiderCounts.ToQueryString());
 
             var dailyCounts = await qDailyCounts.ToListAsync();
+            var riderCounts = await qRiderCounts.ToListAsync();
 
             dashData.Labels = dailyCounts.Select(x => new DateTimeOffset(x.Date)).ToList();
             dashData.LapCounts = dailyCounts.Select(x => Convert.ToDouble(x.Count)).ToList();
+            dashData.RiderCounts = riderCounts.GroupBy(x => x.Date).Select(x => Convert.ToDouble(x.Count())).ToList();
 
             return Ok(dashData);
         }
