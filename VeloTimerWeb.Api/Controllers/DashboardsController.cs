3db5c0a135d7c3e274fd8da870d5df35c9464d5a
Asbjoern L. Johansen <asbjorjo@gmail.com>
2022-01-07 17:59:56 +0100

    Add more items to admin dash

--- VeloTimerWeb.Api/Controllers/DashboardsController.cs
+++ VeloTimerWeb.Api/Controllers/DashboardsController.cs
@@ -9,7 +9,9 @@ using System.Threading.Tasks;
 using VeloTimer.Shared.Models;
 using VeloTimer.Shared.Util;
 using VeloTimerWeb.Api.Data;
+using VeloTimerWeb.Api.Models.Riders;
 using VeloTimerWeb.Api.Models.Statistics;
+using VeloTimerWeb.Api.Models.Timing;
 
 namespace VeloTimerWeb.Api.Controllers
 {
@@ -55,6 +57,13 @@ namespace VeloTimerWeb.Api.Controllers
             var dailyCounts = await qDailyCounts.ToListAsync();
             var riderCounts = await qRiderCounts.ToListAsync();
 
+            dashData.RiderCount = await _context.Set<Rider>().CountAsync();
+            dashData.RiderPublicCount = await _context.Set<Rider>().Where(x => x.IsPublic).CountAsync();
+            dashData.RiderWithTransponderCount = await _context.Set<TransponderOwnership>().Select(x => x.Owner).Distinct().CountAsync();
+            dashData.TransponderCount = await _context.Set<Transponder>().CountAsync();
+            dashData.TransponderWithOwnerCount = await _context.Set<TransponderOwnership>().Select(x => x.Transponder).Distinct().CountAsync();
+            dashData.LapCount = await _context.Set<TransponderStatisticsItem>().Where(x => x.StatisticsItem.StatisticsItem.IsLapCounter).Where(x => x.StartTime > new DateTime(2021, 10, 28, 0, 0, 0)).CountAsync();
+            dashData.PassingCount = await _context.Set<Passing>().Where(x => x.Time > new DateTime(2021, 10, 28, 0, 0, 0)).CountAsync();
             dashData.Labels = dailyCounts.Select(x => new DateTimeOffset(x.Date)).ToList();
             dashData.LapCounts = dailyCounts.Select(x => Convert.ToDouble(x.Count)).ToList();
             dashData.RiderCounts = riderCounts.GroupBy(x => x.Date).Select(x => Convert.ToDouble(x.Count())).ToList();
--- VeloTimerWeb/Shared/Models/AdminDashboardModel.cs
+++ VeloTimerWeb/Shared/Models/AdminDashboardModel.cs
@@ -8,6 +8,13 @@ namespace VeloTimer.Shared.Models
 {
     public class AdminDashboardModel
     {
+        public int RiderCount { get; set; }
+        public int RiderPublicCount { get; set; }
+        public int RiderWithTransponderCount { get; set; }
+        public int TransponderCount { get; set; }
+        public int TransponderWithOwnerCount { get; set; }
+        public int LapCount { get; set; }
+        public int PassingCount { get; set; }
         public List<DateTimeOffset> Labels { get; set; }
         public List<double> LapCounts { get; set; }
         public List<double> RiderCounts { get; set; }
