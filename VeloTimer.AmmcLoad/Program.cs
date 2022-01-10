634df565de66524d3620264a2651c68bab2bf5ec
Asbjoern L. Johansen <asbjorjo@gmail.com>
2022-01-10 17:28:53 +0100

    Use Azure KeyVault outside local development.

--- VeloTimer.AmmcLoad/Program.cs
+++ VeloTimer.AmmcLoad/Program.cs
@@ -1,5 +1,8 @@
+using Azure.Identity;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace VeloTimer.AmmcLoad
 {
@@ -12,6 +15,14 @@ namespace VeloTimer.AmmcLoad
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    if (!context.HostingEnvironment.IsDevelopment())
+                    {
+                        var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("AzureVault"));
+                        config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
