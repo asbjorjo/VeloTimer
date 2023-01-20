25cd796ef3a267d9fa80d3251309751a74373524
Asbjoern L. Johansen <asbjorjo@gmail.com>
2023-01-20 19:19:39 +0100

    Add storage service to track status of passings

---
+++ PassingLoader/Services/Storage/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using PassingLoader.Services.Storage;
+
+namespace VeloTimer.PassingLoader.Services.Storage
+{
+    public static class Startup
+    {
+        public static IServiceCollection ConfigureStorage(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<StorageContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("PgSql"));
+            });
+
+            return services;
+        }
+    }
+}
