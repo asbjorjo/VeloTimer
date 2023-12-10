d8a671a544e34a9cdce9593d985ba5153dd072d0
Asbjoern L. Johansen <asbjorjo@gmail.com>
2023-12-10 21:24:32 +0100

    Send start command when submitter is up

---
+++ VeloTime.Timing.Agents.EventSubmitter/Services/StartupService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using VeloTime.Timing.Contracts;
+using VeloTime.Timing.Services;
+
+namespace VeloTime.Timing.Agents.EventSubmitter.Services
+{
+    public class StartupService : BackgroundService
+    {
+        private readonly IMessagingService _messagingService;
+
+        public StartupService(IMessagingService messagingService) => _messagingService = messagingService;
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var start = new StartLoadingFrom { StartTime = DateTime.UtcNow - TimeSpan.FromHours(-3) };
+            await _messagingService.SendStartLoadingFrom(start);
+        }
+    }
+}
