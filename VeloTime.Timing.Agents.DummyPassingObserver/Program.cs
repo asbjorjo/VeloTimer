2082308fa1cc883164343529eca1a5237868df45
Asbjoern L. Johansen <asbjorjo@gmail.com>
2023-12-10 19:11:22 +0100

    Add dummy implementation of the loader

---
+++ VeloTime.Timing.Agents.DummyPassingObserver/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Hosting;
+using VeloTime.Timing.Agents.PassingObserver;
+
+IHostBuilder hostBuilder = Host
+    .CreateDefaultBuilder(args)
+    .UsePassingObserver();
+
+hostBuilder.Start();
