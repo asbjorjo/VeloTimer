2df7c891d0fe48fa2c56292a861446e7376a0832
Asbjoern L. Johansen <asbjorjo@gmail.com>
2025-09-27 12:45:03 +0200

    Add project for agent

---
+++ src/Agent/HostingExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace VeloTime.Agent;
+
+public static class StartupExtensions
+{
+    private static IHostEnvironment? _env;
+    public static IHostApplicationBuilder ConfigureServices(this IHostApplicationBuilder builder)
+    {
+        var services = builder.Services;
+        var configuration = builder.Configuration;
+        _env = builder.Environment;
+
+        services.AddSerilog(lc => lc
+            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+            .Enrich.FromLogContext()
+            .ReadFrom.Configuration(configuration)
+            );
+
+        return builder;
+    }
+}
