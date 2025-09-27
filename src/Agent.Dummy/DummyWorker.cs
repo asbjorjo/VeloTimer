9a694e933c10d898582f6ba78ddde3ff7d8db472
Asbjoern L. Johansen <asbjorjo@gmail.com>
2025-09-27 12:54:59 +0200

    Add agent with dummy implementation and a timing module

---
+++ src/Agent.Dummy/DummyWorker.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeloTime.Agent.Dummy
+{
+    internal class DummyWorker : IHostedService
+    {
+        private ILogger<DummyWorker> logger;
+
+        public DummyWorker(ILogger<DummyWorker> logger)
+        {
+            this.logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Dummy Worker started");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Dummy Worker stopped");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Agent.Dummy/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using VeloTime.Agent;
+using VeloTime.Agent.Dummy;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// Configure core agent services
+builder.ConfigureServices();
+
+
+// Add services for specific system
+builder.Services.AddHostedService<DummyWorker>();
+
+var app = builder.Build();
+
+await app.RunAsync();
