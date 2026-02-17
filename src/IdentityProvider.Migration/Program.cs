67266362316d30704916c7ed9ab0d6aa3cbb153a
Asbjørn L. Johansen <asbjorjo@gmail.com>
2026-02-17 10:48:14 +0100

    chore: Delete src/IdentityProvider.Migration directory

--- src/IdentityProvider.Migration/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = Host.CreateApplicationBuilder(args);
-
-builder.AddServiceDefaults();
-builder.ConfigureServices();
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
