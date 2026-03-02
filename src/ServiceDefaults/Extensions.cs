a5788ff47003cd18324c48942e34eb40553192e9
Asbjoern L. Johansen <asbjorjo@gmail.com>
2026-03-02 20:47:12 +0100

    fix: Remove DNS SRV service discovery

--- src/ServiceDefaults/Extensions.cs
+++ src/ServiceDefaults/Extensions.cs
@@ -24,10 +24,10 @@ namespace Microsoft.Extensions.Hosting
 
             builder.AddDefaultHealthChecks();
 
-            builder.Services.AddDnsSrvServiceEndpointProvider(options =>
-            {
-                options.QuerySuffix = "velotime-dev.svc.cluster.local";
-            });
+            //builder.Services.AddDnsSrvServiceEndpointProvider(options =>
+            //{
+            //    options.QuerySuffix = "velotime-dev.svc.cluster.local";
+            //});
             builder.Services.AddConfigurationServiceEndpointProvider();
             builder.Services.AddPassThroughServiceEndpointProvider();
             builder.Services.AddServiceDiscoveryCore();
