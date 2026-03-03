4c01816a3d06fd966bb2d089130c08f3ff12f542
Asbjoern L. Johansen <asbjorjo@gmail.com>
2026-03-03 19:43:48 +0100

    fix: Apply https to development

--- src/WebUI.Mud/Program.cs
+++ src/WebUI.Mud/Program.cs
@@ -102,15 +102,15 @@ app.UseForwardedHeaders();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseWebAssemblyDebugging();
-}
-else
-{
     app.Use((context, next) =>
     {
         context.Request.Scheme = "https";
         return next(context);
     });
+    //app.UseWebAssemblyDebugging();
+}
+else
+{
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
