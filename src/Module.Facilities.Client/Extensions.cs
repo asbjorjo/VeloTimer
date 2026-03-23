d9cf32ff293a0d444385f190384e7774361770d1
Asbjoern L. Johansen <asbjorjo@gmail.com>
2026-03-23 22:01:29 +0100

    feat: Generate OpenAPI specs at build

---
+++ src/Module.Facilities.Client/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using VeloTime.Module.Facilities.Client;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class Extensions
+{
+    public static IHttpClientBuilder AddFacilitiesClient(this IServiceCollection services)
+    {
+        return services.AddHttpClient<IFacilitiesClient, FacilitiesClient>(
+            static client => client.BaseAddress = new("https+http://module-facilities-api"));
+    }
+}
